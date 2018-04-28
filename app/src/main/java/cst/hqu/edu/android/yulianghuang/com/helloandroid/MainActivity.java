11e44815dce217d5bba23cf18633bde692ed75c8
yulianghuang <2977621972@qq.com>
2018-04-28 18:59:18 +0800

    第一次提交

---
+++ app/src/main/java/cst/hqu/edu/android/yulianghuang/com/helloandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package cst.hqu.edu.android.yulianghuang.com.helloandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
